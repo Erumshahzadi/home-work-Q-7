71789168f286dba4603ead8959aeac089d594d82
Erum shahzadi <163527258+Erumshahzadi@users.noreply.github.com>
2024-04-16 11:26:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// Q9: Write a simple JavaScript program that checks if a person is eligible for a CNIC (show a message with the person's name).
+let personAge = 22;
+let personName = "Erum Shahzadi";
+if (personAge >= 18) {
+    console.log(`${personName} is eligible for CNIC`);
+}
+else {
+    console.log(`Sorry, you are not eligible`);
+}
+export {};
---
+++ main.ts
@@ -0,0 +1,15 @@
+// Q9: Write a simple JavaScript program that checks if a person is eligible for a CNIC (show a message with the person's name).
+
+
+let personAge:number=22;
+let personName:string="Erum Shahzadi";
+
+
+
+if (personAge>=18){
+
+    console.log(`${personName} is eligible for CNIC`);
+}
+else{
+    console.log (`Sorry, you are not eligible`);
+}
